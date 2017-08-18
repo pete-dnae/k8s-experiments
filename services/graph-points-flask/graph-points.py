ea10a97455272bb7df38952af5857e41545765b8
pete-dnae <pete.howard@dnae.com>
2017-08-18 18:14:09 +0100

    :q

--- services/graph-points-flask/graph-points.py
+++ services/graph-points-flask/graph-points.py
@@ -1,16 +1,18 @@
 """
-A Flask app that replies to all requests with the hello world string.
+A Flask app that replies to all requests with a json object that encapsulates a
+series of hard-coded x,y point values.
 
-In accordance with 12-factor principles, we don't hook up the app to a web 
+In accordance with 12-factor principles, we don't hook up the app to the web 
 server (like the Flask development server) in this code (nor try to run it) - 
 and instead, leave these choices to be done by external configuration. 
 
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def graph_points():
-    return 'Hello World'
+    points = ((0,1), (1, 0.5), (2, 1.0))
+    return jsonify(points)
