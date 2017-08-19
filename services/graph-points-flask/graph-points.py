897ae94321dce4fb2e856814fa5c7d35f42619d0
pete-dnae <pete.howard@dnae.com>
2017-08-19 21:40:02 +0100

    Use jsonschema to validate the requests' JSON data.

--- services/graph-points-flask/graph-points.py
+++ services/graph-points-flask/graph-points.py
@@ -1,9 +1,9 @@
 """
-A Flask REST API that replies to all POST requests with a JSON object that 
-represents a graph of a sin wave. I.e. a list of x/y points.
+A Flask REST API that replies to POST requests, regardless of path, with a 
+JSON object that represents a graph of a sin wave. I.e. a list of x/y points.
 
-The obligatory POST payload determines how finely spaced the points should be.
-Like this:
+How finely-spaced the points are is determined from the request's POST JSON
+payload. Like this.
 
 {
     "intervals": 12
@@ -11,22 +11,35 @@ Like this:
 
 """
 
-from flask import Flask, jsonify, abort, request
 import math
 
+from flask import Flask, jsonify, abort, request
+from jsonschema import validate
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST'])
 def graph_points():
 
     # Get the point spacing from the POST message.
+
+    # Catching both absence of POST data, and schema violations.
     try:
         json = request.get_json()
+        validate(json, {
+            "type": "object",
+            "properties": {
+                "intervals": {
+                    "type": "integer",
+                    "minimum": 8,
+                    "maximum": 200,
+                }
+            }
+        })
         intervals = json['intervals']
     except Exception as e:
-        abort(400, 'Cannot find [intervals] key in JSON POST payload')
-    if ((intervals < 3) or (intervals > 100)):
-        abort(400, 'Illegal value for [intervals] parameter')
+        abort(400, e)
 
     # The other sin wave parameters are hard coded.
     ampl = 2
