3d8a3b865e35ad82de8bb44b1329e7e80a79cccb
pete-dnae <pete.howard@dnae.com>
2017-09-11 23:02:18 +0100

    Added unit test module for graph_points_flask.

--- services/graph_points_flask/graph_points_flask_test.py
+++ services/graph_points_flask/graph_points_flask_test.py
@@ -13,13 +13,33 @@ class GraphPointsTestCase(unittest.TestCase):
         self.app = graph_points_flask.app.test_client()
 
 
-    def test_something(self):
+    def test_points_are_correct(self):
         response=self.app.post('/', 
-            data = json.dumps(dict(intervals=12)), 
+            data = json.dumps({'intervals':12}), 
             content_type='application/json')
 
-        print('response object is: %s\n' % response)
-        print('response data is: %s\n' % response.data)
+        payload = json.loads(response.data.decode())
+        self.assertEqual(len(payload), 13)
+        x, y = payload[0]
+        self.assertAlmostEqual(x, 0, 3)
+        self.assertAlmostEqual(y, 0, 3)
+        x, y = payload[7]
+        self.assertAlmostEqual(x, 10.996, 3)
+        self.assertAlmostEqual(y, -2, 3)
+
+    def test_400_error_generated_for_malformed_request(self):
+        response=self.app.post('/', 
+            data = 'garbage',
+            content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_message_from_failed_schema_validation_is_correct(self):
+        out_of_range_intervals = 2
+        response=self.app.post('/', 
+            data = json.dumps({'intervals':out_of_range_intervals}), 
+            content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue(b'is less than the minimum of 8' in response.data)
 
 
 if __name__ == '__main__':
